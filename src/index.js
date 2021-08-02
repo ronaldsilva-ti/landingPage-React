17921c59aa3e5a2abb4f54efc241ec71ecc53070
ronaldsilva-ti <alvesronald@yahoo.com>
2021-08-02 07:06:57 -0300

    added testing style in Home

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import { GlobalStyles } from './styles/global-styles';
 import Home from './templates/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,7 +1,11 @@
+import * as Styled from './styles';
+
 function Home() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Styled.Wrapper>
+        <h1>Hello</h1>
+      </Styled.Wrapper>
     </div>
   );
 }
